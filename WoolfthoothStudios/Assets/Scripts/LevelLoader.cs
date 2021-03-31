a0253ced053b51e31a9183bd7c4ee708855da08b
mMomarMm <mMomarMm@users.noreply.github.com>
2021-03-30 23:52:49 -0600

    loading scene 0.1

---
+++ WoolfthoothStudios/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+
+    }
+}
